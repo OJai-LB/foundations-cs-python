02df0582ac5a45de0ed9b2080e6d4632ba6f51ba
Omar Jarjour <omarjarjour.lb@gmail.com>
2023-11-01 09:40:42 +0200

    Done, and tested the get Average Score method.

--- assignment_03_Omar_Jarjour.py
+++ assignment_03_Omar_Jarjour.py
@@ -1,1 +1,11 @@
 def GetAverageScore(data_frame):
+    data_frame_average = {}
+    for i in data_frame:
+        if "Name" in i and "Score" in i:
+            data_frame_average[i["Name"]] = sum(i["Score"]) / len(i["Score"])
+    return data_frame_average
+
+data_frame= [{"Name": "Omar", "Age": 23, "Score": (89, 90, 99)}, {"Name": "Ahmad", "Age": 101, "Score": (50, 50, 50)}]
+
+print(data_frame)
+print(GetAverageScore(data_frame))
