f7a581d0e1f8aea36a6b515b5f5226738f05905a
Omar Jarjour <omarjarjour.lb@gmail.com>
2023-11-01 17:56:59 +0200

    done till choice 3 base functions

--- assignment_03_Omar_Jarjour.py
+++ assignment_03_Omar_Jarjour.py
@@ -5,7 +5,24 @@ def GetAverageScore(data_frame):
             data_frame_average[i["Name"]] = sum(i["Score"]) / len(i["Score"])
     return data_frame_average
 
-data_frame= [{"Name": "Omar", "Age": 23, "Score": (89, 90, 99)}, {"Name": "Ahmad", "Age": 101, "Score": (50, 50, 50)}]
+def GetYoungestStudent(data_frame):
+    data_frame_sorted = sorted(data_frame, key=lambda student: student["Age"])
+    return data_frame_sorted[0]["Name"]
 
-print(data_frame)
-print(GetAverageScore(data_frame))
+
+def GetHighestScore(data_frame):
+    data_frame_total_score = {}
+    for i in data_frame:
+        if "Name" in i and "Score" in i:
+            data_frame_total_score[i["Name"]] = sum(i["Score"])
+    highest_valued_score_student = []
+    highest_valued_key = sorted(list(data_frame_total_score.values()), reverse=True)[0]
+    for key in data_frame_total_score:
+        if data_frame_total_score[key] == highest_valued_key:
+            highest_valued_score_student.append(key)
+
+
+    return highest_valued_score_student
+
+data_frame= [{"Name": "Omar", "Age": 23, "Score": (100, 100, 100)}, {"Name": "Ahmad", "Age": 101, "Score": (100, 100, 100)}]
+print(GetHighestScore(data_frame))
